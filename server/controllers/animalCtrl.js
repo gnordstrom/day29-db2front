2d158d5b34aa93a628251ae46bd74179b811c891
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-09-22 11:31:57 -0600

    second commit

--- server/controllers/animalCtrl.js
+++ server/controllers/animalCtrl.js
@@ -1,3 +1,14 @@
-module.exports = {
+var app = require('../server');
+var db = app.get('db')
 
+module.exports = {
+    getAllAnimals: function(req, res) {
+      db.get_all_animals(function(err, animals) {
+        if (err) {
+          res.status(402).json(err);
+        } else {
+        res.status(200).json(animals); 
+      }
+    });
+  }
 };
---
+++ server/db/post_an_animal.sql
@@ -0,0 +1,3 @@
+insert into animals
+  (name, limbs, tail, habitat)
+values ($1, $2, $3, $4);
